3d2d01917e7ae38c9a86b9bbb4f31e48f7a948da
Nishtha <nishtha@example.com>
2026-03-23 18:25:56 +0530

    Commit by Nishtha

--- harmoniq-backend/src/main/java/com/harmoniq/backend/repository/ConcertRepository.java
+++
@@ -1,8 +0,0 @@
-package com.harmoniq.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.harmoniq.backend.model.Concert;
-
-public interface ConcertRepository extends JpaRepository<Concert, Long> {
-}
