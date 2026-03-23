b85286bcc40a0427d506051cb2f765c0bbf4593c
Janavi <janavi@example.com>
2026-03-23 18:36:09 +0530

    Commit by Janavi

--- harmoniq-backend/src/main/java/com/harmoniq/backend/repository/RSVPRepository.java
+++
@@ -1,7 +0,0 @@
-package com.harmoniq.backend.repository;
-
-import com.harmoniq.backend.model.RSVP;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RSVPRepository extends JpaRepository<RSVP, Long> {
-}
